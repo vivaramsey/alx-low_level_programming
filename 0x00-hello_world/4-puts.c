a3a5d5bbe70cc66972106523c136f5830b588288
Rahmetu <rahmim403@gmail.com>
2021-08-26 08:46:55 -0400

    4puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- *main - Entry point, primary function that must be at the beginning of the program
- *Return:0 - Always 0 (Success), on reaching the end the program should return 0
+ *main - primary function that must be at the beginning of the program
+ *Return:0 - Always 0 (Success)
  *
  */
 
