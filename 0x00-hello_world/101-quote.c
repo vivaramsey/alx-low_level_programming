8fa9c7cf0cebf6b157734694c954bdcd16cec4b3
Rahmetu <rahmim403@gmail.com>
2021-08-26 08:55:19 -0400

    betty style no error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -12,10 +12,10 @@
 int main(void)
 {
 int w;
-w = write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 10);
+w = write(STDOUT_FILENO, "and that is useful\" - Dora, 2015-10-19\n", 10);
 if (w < 0)
-  {
-    perror("Writing error: ");
-  }
+{
+perror("Writing error:");
+}
 return (1);
 }
