fb363237d7ee2ac7dccdcab35f8f4e0c2c2aa2a8
Rahmetu <rahmim403@gmail.com>
2021-09-01 14:16:17 -0400

    fixing code

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,44 +1,62 @@
+#include "holberton.h"
 #include "main.h"
+
 /**
- *print_times_table - n times table.
- *@n: the number defines the table.
- *
- *Return: no return value
+ * tenth - print time table
+ * @res: number of the time table
+ * Return: void none
  */
+
+void tenth(int res)
+{
+	_putchar(',');
+	_putchar(' ');
+	_putchar(' ');
+	_putchar(res / 10 + '0');
+	_putchar(res % 10 + '0');
+}
+
+/**
+  * print_times_table - print time table
+  * @n: number of the time table
+  * Return: void none
+  */
+
 void print_times_table(int n)
 {
-	int num, mult, prod;
+	int i;
+	int j;
+	int res;
 
-	if (n >= 0 && n <= 15)
+	if (n > 15 || n < 0)
+		return;
+	for (i = 0; i <= n; i++)
 	{
-		for (num = 0; num <= n; num++)
+		for (j = 0; j <= n; j++)
 		{
-			_putchar('0');
-
-			for (mult = 1; mult <= n; mult++)
+			res = i * j;
+			if (res <= 9)
+			{
+				if (j > 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+				}
+				_putchar(res + '0');
+			}
+			if (res > 9 && res <= 99)
+				tenth(res);
+			if (res > 99 && res <= 999)
 			{
 				_putchar(',');
 				_putchar(' ');
-
-				prod = num * mult;
-
-				if (prod <= 99)
-					_putchar(' ');
-				if (prod <= 9)
-					_putchar(' ');
-
-				if (prod >= 100)
-				{
-					_putchar((prod / 100) + '0');
-					_putchar(((prod / 10)) % 10 + '0');
-				}
-				else if (prod <= 99 && prod >= 10)
-				{
-					_putchar((prod / 10) + '0');
-				}
-				_putchar((prod % 10) + '0');
+				_putchar(res / 100 + '0');
+				_putchar((res % 100) / 10 + '0');
+				_putchar(res % 10 + '0');
 			}
-			_putchar('\n');
 		}
+		_putchar('\n');
 	}
 }
