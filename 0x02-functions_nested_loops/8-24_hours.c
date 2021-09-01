9cba66b02ca1ea47f9ed6c1ad7c0e05fcb489d72
Rahmetu <rahmim403@gmail.com>
2021-09-01 13:47:55 -0400

    creted other files

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * jack_bauer - print 24hours time frame
+ */
+void jack_bauer(void)
+{
+	int hr = 0, mn = 0;
+
+	for (; hr < 24; mn++)
+	{
+		if (mn == 60)
+		{
+			hr++;
+			mn = 0;
+		}
+
+		if (hr == 24)
+			break;
+
+		_putchar((hr / 10) + 48);
+		_putchar((hr % 10) + 48);
+		_putchar(58);
+		_putchar((mn / 10) + 48);
+		_putchar((mn % 10) + 48);
+		_putchar(10);
+	}
+}
